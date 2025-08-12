f191d53a52f9cd78ed6466376f87fdd9548b5aa6
Kerstin Wiessmann <kwiessmann@gmail.com>
2025-08-12 20:31:37 +0200

    updated link

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-          <Weather />
+        <Weather />
         <footer>
           This project was coded by Kerstin Wiessmann and is{" "}
           <a
             href="https://github.com/KerstinWieseline/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
