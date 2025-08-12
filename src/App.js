ff75383d94a3624210af9016731a2a67f12e6fdd
Kerstin Wiessmann <kwiessmann@gmail.com>
2025-08-12 19:38:34 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kerstin Wiessmann and is{" "}
-        <a
-          href="https://github.com/KerstinWieseline/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kerstin Wiessmann and is{" "}
+          <a
+            href="https://github.com/KerstinWieseline/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
