b416a412fc708e4fa5aa23966c3803836a06a636
Kerstin Wiessmann <kwiessmann@gmail.com>
2025-08-12 19:22:28 +0200

    Added the GitHub Repo url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kerstin Wiessmann and is{" "}
+        <a
+          href="https://github.com/KerstinWieseline/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
