35285388596b061f7114087728abe424f8cf308d
Kerstin Wiessmann <kwiessmann@gmail.com>
2025-08-16 19:06:28 +0200

    updated Footer

--- src/App.js
+++ src/App.js
@@ -9,13 +9,21 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Berlin" />
         <footer>
-          This project was coded by Kerstin Wiessmann and is{" "}
+          This project was coded by Kerstin Wiessmann, it is{" "}
           <a
             href="https://github.com/KerstinWieseline/react-weather-app"
             target="_blank"
             rel="noreferrer"
           >
             open-sourced on GitHub
+          </a>
+          and hosted on{" "}
+          <a
+            href="https://kerstin-weather-react.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify!
           </a>
         </footer>
       </div>
